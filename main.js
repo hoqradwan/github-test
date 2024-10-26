15bcf8a5c0b805f46587b72751a1b46109fdadb4
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2024-10-26 22:32:47 +0600

    north

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 console.log("sabbir bibahito");
 console.log("redawan singleish");
 console.log("sabbir khawa mal");
-console.log("sabbir east west");
-console.log('laga laga pori moni k laga');
-
+console.log("sabbir north south");
+console.log("laga laga pori moni k laga");
